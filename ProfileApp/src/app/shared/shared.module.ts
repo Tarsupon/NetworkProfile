fde82f170b85b20e135f74e7543aa97749dc9f26
Tarsupon <alexeyka981@gmail.com>
2019-07-15 14:29:37 +0300

    Init commit, create feature based structure

---
+++ ProfileApp/src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from "@angular/material";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+  ]
+})
+export class SharedModule { }
